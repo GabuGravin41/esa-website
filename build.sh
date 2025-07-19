927de91d1c6b0310d6a2f0ec7eb79458bcc0a446
GabuGravin41 <daltonomondi588@gmail.com>
2025-07-19 20:31:31 +0300

    update and try to deploy

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser if it doesn't exist (optional - you can remove this)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'esa.kenyattauniv@gmail.com', 'Nairobi100!') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell 
